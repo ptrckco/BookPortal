93e83f947bdfa3867aff71099debdf7e00ff87d3
ptrckco <54984368+ptrckco@users.noreply.github.com>
2020-10-19 23:08:09 +1100

    Create AdminBookList.java

---
+++ src/booklist/model/bean/AdminBookList.java
@@ -0,0 +1,109 @@
+package booklist.model.bean;
+
+import java.sql.Timestamp;
+
+public class AdminBookList {
+	
+	protected int bid;
+    protected int cid;
+    protected String booktitle;
+    protected String description;
+    protected String author;
+    protected Timestamp publisheddate;
+    protected String isbn;
+    protected float price;
+    protected int noofpages;
+
+
+    public AdminBookList() {}
+    
+    public AdminBookList(String booktitle, int cid, String description, String author, Timestamp publisheddate, String isbn, float price, int noofpages) {
+        this.cid = cid;
+        this.booktitle = booktitle;
+        this.description = description;
+        this.author = author;
+        this.publisheddate = publisheddate;
+        this.isbn = isbn;
+        this.price = price;
+        this.noofpages = noofpages;
+        
+    }
+
+    public AdminBookList(int bid, String booktitle, int cid, String description, String author, Timestamp publisheddate, String isbn, float price, int noofpages) {
+    	this.bid = bid;
+        this.cid = cid;
+        this.booktitle = booktitle;       
+        this.description = description;
+        this.author = author;
+        this.publisheddate = publisheddate;
+        this.isbn = isbn;
+        this.price = price;
+        this.noofpages = noofpages;
+        
+    }
+
+
+    public int getbid() {
+        return bid;
+    }
+    public void setbid(int bid) {
+        this.bid = bid;
+    }
+    
+    public int getcid() {
+        return cid;
+    }
+    public void setcid(int cid) {
+        this.cid = cid;
+    }
+    
+    public String getbooktitle() {
+        return booktitle;
+    }
+    public void setbooktitle(String booktitle) {
+        this.booktitle = booktitle;
+    }
+    
+    public String getdescription() {
+        return description;
+    }
+    public void setdescription(String description) {
+        this.description = description;
+    }
+    
+    public String getauthor() {
+        return author;
+    }
+    public void setauthor(String author) {
+        this.author = author;
+    }
+    
+    public Timestamp getpublisheddate() {
+        return publisheddate;
+    }
+    public void setpublisheddate(Timestamp publisheddate) {
+        this.publisheddate = publisheddate;
+    }
+    
+    public String getisbn() {
+        return isbn;
+    }
+    public void setisbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public float getprice() {
+        return price;
+    }
+    public void setprice(float price) {
+        this.price = price;
+    }
+    
+    public int getnoofpages() {
+        return noofpages;
+    }
+    public void setnoofpages(int noofpages) {
+        this.noofpages = noofpages;
+    }
+
+}
