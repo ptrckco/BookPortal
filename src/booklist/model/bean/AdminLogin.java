5cb3c62996e3d8623baca3c3e2a2d97ddfd9ffda
ptrckco <54984368+ptrckco@users.noreply.github.com>
2020-10-19 23:08:45 +1100

    Create AdminLogin.java

---
+++ src/booklist/model/bean/AdminLogin.java
@@ -0,0 +1,25 @@
+package booklist.model.bean;
+
+public class AdminLogin {
+	
+		private String uname;
+		private String pws;
+
+		public String getUname() {
+			return uname;
+		}
+
+		public void setUname(String uname) {
+			this.uname = uname;
+		}
+
+		public String getPws() {
+
+			return pws;
+		}
+
+		public void setUpassword(String pws) {
+
+			this.pws = pws;
+		}
+}
